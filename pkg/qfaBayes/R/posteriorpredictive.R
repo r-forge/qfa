2538c245cca531172b804eea61faccfe5fe4dd69
jheydari (Mr ) <j.j.heydari@ncl.ac.uk>
2011-06-13 11:23:07 +0000

    Adding first version of qfaBayes package, to test if it can load qfa, rjags etc.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qfa@60 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/qfaBayes/R/posteriorpredictive.R
@@ -0,0 +1,87 @@
+
+funcPostPred<-function(iter,work,QFA){
+
+N<-QFA$N
+M<-QFA$M
+NoSum<-QFA$NoSum
+NoORF<-QFA$NoORF
+NoTime<-QFA$NoTime
+
+K_s<-QFA$K_s
+r_s<-QFA$r_s
+PO_s<-QFA$PO_s
+beta<-QFA$beta
+tau_s<-QFA$tau_s
+delta<-QFA$delta
+alpha_ij_sd<-QFA$alpha_ij_sd
+gamma_ij_sd<-QFA$gamma_ij_sd
+alpha<-QFA$alpha
+gamma<-QFA$gamma
+alpha_i<-QFA$alpha_i
+gamma_i<-QFA$gamma_i
+alpha_ij<-QFA$alpha_ij
+gamma_ij<-QFA$gamma_ij
+
+namesamp<-QFA$namesamp
+K<-QFA$K
+K_i<-QFA$K_i
+K_ij<-QFA$K_ij
+PO<-QFA$PO
+k_tau<-QFA$k_tau
+r<-QFA$r
+r_i<-QFA$r_i
+r_ij<-QFA$r_ij
+r_tau<-QFA$r_tau
+taui<-QFA$taui
+tau<-QFA$tau
+
+
+
+
+
+postpred<-matrix(0,iter,length(namesamp))
+
+postpred[,1]<-rgamma(iter,(K_s^2)/alpha^2,K_s/alpha^2)
+
+for (i in 1:length(K_i)){
+j=(2)+i-1
+postpred[,j]<-rgamma(iter,(K^2)/alpha_i^2,K/alpha_i^2)
+}
+
+t=1
+for (i in (N+2):(M+N+1) ){
+if(((i-(N+2))==c(NoSum[-1])[t])) t=t+1
+postpred[,i]<-rgamma(iter,(K_i[t]^2)*k_tau[t],K_i[t]*k_tau[t])
+}
+postpred[,(M+N+2)]<-runif(iter,PO_s,beta)
+
+for (i in 1:length(k_tau)){
+j=(M+N+3)+i-1
+postpred[,j]<-rgamma(iter,(alpha_ij^2)/alpha_ij_sd^2,(alpha_ij^2)/alpha_ij_sd^2)
+}
+
+postpred[,(M+2*N+3)]<-rgamma(iter,(r_s^2)/gamma^2,r_s/gamma^2)
+
+for (i in 1:length(r_i)){
+j=(M+2*N+4)+i-1
+postpred[,j]<-rgamma(iter,(r^2)/gamma_i^2,r/gamma_i^2)
+}
+t=1
+for (i in (M+2*N+4):(M+3*N+3) ){
+if(((i-(M+2*N+4))==c(NoSum[-1])[t])) t=t+1	
+postpred[,i]<-rgamma(iter,(r_i[t]^2)/gamma_ij^2,r_i[t]/gamma_ij^2)
+}
+
+for (i in 1:length(r_tau)){
+j=(2*M+3*N+4)+i-1
+postpred[,j]<-rgamma(iter,(gamma_ij^2)/gamma_ij_sd^2,(gamma_ij_sd^2)/alpha_ij_sd^2)
+}
+
+for (i in 1:length(taui)){
+j=(2*M+4*N+4)+i-1
+postpred[,j]<-rgamma(iter,(tau^2)/delta^2,tau/delta^2)
+}
+
+postpred[,(2*M+5*N+4)]<-rgamma(iter,(tau_s^2)/delta^2,tau_s/delta^2)
+postpred
+}
