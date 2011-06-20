a0987aa44af7c5fd5e8165973d6fc00dbbf02a49
jheydari <jheydari@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-20 14:30:34 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qfa@75 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/qfaBayes/demo/Joint.R
+++ pkg/qfaBayes/demo/Joint.R
@@ -6,11 +6,11 @@ DescripControl<-"ExptDescriptionCDC13.txt"
 upd=2000
 iter=1000
 thin=1
-wrk=Work
+work="Joint"
 PlotOutput=FALSE
 a=rod.read(file=Control,inoctimes=DescripControl)
 qfa.variables(a)
-Screen<-as.character(unique(a$Screen.Name))
+Screen<-as.character(unique(a$Screen.Name))[1]
 Treat<-as.character(unique(a$Treatment))[2]
 MPlate<-unique(a$MasterPlate.Number)[15]
 a<-funcREMOVE(a,Screen,Treat,MPlate)
@@ -19,18 +19,18 @@ a<-funcREMOVE(a,Screen,Treat,MPlate)
 Query<-c("cdc13-1_rad9D_SDLv2_Rpt1.txt","cdc13-1_rad9D_SDLv2_Rpt2.txt","cdc13-1_rad9D_SDLv2_Rpt3.txt","cdc13-1_rad9D_SDLv2_Rpt4.txt")
 Work="testjoint"
 DescripQuery<-"ExptDescriptionCDC13RAD9.txt"
-upd=2000
-iter=1000
+upd=2
+iter=5
 thin=1
 wrk=Work
 PlotOutput=FALSE
 b=rod.read(file=Query,inoctimes=DescripQuery)
 qfa.variables(b)
-Screen<-as.character(unique(b$Screen.Name))
+Screen<-as.character(unique(b$Screen.Name))[1]
 b<-funcREMOVE(b,Screen,Treat,MPlate)
 
-JointFit<-qfa.Joint(a,b,Scaling=TRUE,iter,upd,thin,PlotOutput=TRUE,work,CustomModel=FALSE)
-
+JointFit<-qfa.Joint(a,b,Scaling=TRUE,iter,upd,thin,PlotOutput=FALSE,work,CustomModel=FALSE)
+QFA.J.Plots(work,JointFit)
 
 # eof
 
