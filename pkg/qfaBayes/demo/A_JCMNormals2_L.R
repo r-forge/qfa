53c75e6e78567751382da9ec86b9dd244458223f
jheydari <jheydari@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-30 09:17:48 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qfa@102 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/qfaBayes/demo/A_JCMNormals2_L.R
+++ pkg/qfaBayes/demo/A_JCMNormals2_L.R
@@ -3,7 +3,7 @@
 upd=1000000
 iter=1000000
 thin=100
-work="Joint"
+work="Joint_L"
 PlotOutput=FALSE
 data("Adam_cdc-1_SDLV2_REP1")
 qfa.variables(a)
@@ -20,7 +20,7 @@ Screen<-as.character(unique(b$Screen.Name))[1]
 b<-funcREMOVE(b,Screen,Treat,MPlate)
 CustomModel="A_JCMNNormals2"
 JointFit<-qfa.Joint(a,b,Scaling=TRUE,iter,upd,thin,PlotOutput=FALSE,work,CustomModel=FALSE)
-save(ControlFit,file=paste(CustomModel,"BASIC","R",sep="."))
+save(ControlFit,file=paste(CustomModel,work,"R",sep="."))
 
 qfaplots.J(JointFit,work)
 
