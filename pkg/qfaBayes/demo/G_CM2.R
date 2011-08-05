046ad88710f37005d8b12d0958661c8298b8636f
jheydari (Mr ) <j.j.heydari@ncl.ac.uk>
2011-08-05 13:23:26 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qfa@129 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/qfaBayes/demo/G_CM2.R
+++ pkg/qfaBayes/demo/G_CM2.R
@@ -9,7 +9,7 @@ source("iter.R")
 #iter=2
 #thin=2
 work=paste(upd,iter,thin,sep="_")
-data("AdamFull")
+data("AdamFull8Rep")
 qfa.variables(a)
 Screen<-as.character(unique(a$Screen.Name))
 Treat<-27
@@ -18,6 +18,15 @@ a<-funcREMOVE(a,Screen,Treat,MPlate)
 
 inits=list('K'=log(0.089),'r'=log(1.82),'PO_L'=log(0.00224),'K_i_tau_L'=1.25,'r_i_tau_L'=3.429)
 
+
+a<-a[!(a$Col==24),]
+a<-a[!(a$Col==1),]
+a<-a[!(a$Row==1),]
+a<-a[!(a$Row==16),]
+
+vec<-a[a$ORF=="YOR202W",][1:399,]#peter 392 adam 399
+a<-a[!(a$ORF=="YOR202W"),]
+a<-rbind(a,vec)
 
 a<-funcIDORDER(a)
 IDuni<-unique(a$ID)
@@ -30,6 +39,7 @@ if (vec[j]<vec[j-1]){t=1}
 }
 if (t==1){a<-a[!a$ID==IDuni[i],]}
 t=0
+print(i*100/N)
 }
 
 ControlFit<-qfa.Hierachical(a,Scaling=TRUE,iter=iter,upd=upd,thin=thin,PlotOutput=FALSE,work="ModelHExample",CustomModel=CustomModel,inits=inits)
