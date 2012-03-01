2ff7cb3661cbc1085f55d0e6ad3ae9b2cb16bed8
conor (Dr ) <conor.lawless@ncl.ac.uk>
2012-03-01 18:51:54 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qfa@147 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/qfa/R/Exponential.R
+++ pkg/qfa/R/Exponential.R
@@ -18,7 +18,10 @@ slopeFun=function(x,y,N){
 	
 }
 
-lin.fit<-function(tim,growth,inocguess=1,xybounds=1,inits=list(),logTransform=FALSE,dplot=FALSE,asking=FALSE,orf="",...){
+lin.fit<-function(tim,growth,inocguess=1,xybounds=1,inits=list(),logTransform=FALSE,dplot=FALSE,asking=FALSE,orf="",detectThresh=0,...){
+#dplot=TRUE
+#asking=TRUE
+
 	x=tim
 	y=growth
 	x=x[y>0]
@@ -30,25 +33,28 @@ lin.fit<-function(tim,growth,inocguess=1,xybounds=1,inits=list(),logTransform=FA
 	cgrow<<-growth
 
 	slopecutoff=0
-	ldenscutoff=1e-2
+	ldenscutoff=detectThresh
 	#udenscutoff=0.07 
-	udenscutoff=0.5*Kest
+	udenscutoff=0.2*Kest
 	
 	xnew=x[(s>slopecutoff)&(y>log(ldenscutoff))&(y<log(udenscutoff))]
 	ynew=y[(s>slopecutoff)&(y>log(ldenscutoff))&(y<log(udenscutoff))]
 	snew=s[(s>slopecutoff)&(y>log(ldenscutoff))&(y<log(udenscutoff))]
 	
 	pars=c(Kest,0,inocguess,1,1)
-	try({
-	# Fit straight line to filtered data
-	linfit=lm(ynew~1+xnew)
-	lA0=as.numeric(linfit$coefficients[1])
-	r=as.numeric(linfit$coefficients[2])
-	A0=exp(lA0)
-	if((is.na(A0))|(is.na(r))) {r=0; A0=inocguess; Kest=inocguess}
+	#try({
+	if(length(xnew)>1){
+		# Fit straight line to filtered data
+		linfit=lm(ynew~1+xnew)
+		lA0=as.numeric(linfit$coefficients[1])
+		r=as.numeric(linfit$coefficients[2])
+		A0=exp(lA0)
+	}else{
+		r=0; A0=inocguess; Kest=inocguess; lA0=log(A0)
+	}
+	if((is.na(A0))|(is.na(r))) {r=0; A0=inocguess; Kest=inocguess; lA0=log(A0)}
 	pars=c(Kest,r,A0,1,1)
-	})
-	
+
 	if (dplot)	{
 		par(ask = asking)
 		dt=signif(24*log(2)/r,3)
@@ -58,7 +64,7 @@ lin.fit<-function(tim,growth,inocguess=1,xybounds=1,inits=list(),logTransform=FA
 		op=par(mfrow=c(2,1))
 		plot(x,y,xlab="Time (d)",ylab="log cell density (AU)",main="Density cutoffs (green)")
 		points(xnew,ynew,col="red")
-		abline(h=log(ldenscutoff),col="green")
+		#abline(h=log(ldenscutoff),col="green")
 		abline(h=log(udenscutoff),col="green")
 		plot(x,s,xlab="Time (d)",ylab="slope of log density (AU)",main="Slope cutoff (blue)")
 		points(xnew,snew,col="red")
@@ -68,9 +74,11 @@ lin.fit<-function(tim,growth,inocguess=1,xybounds=1,inits=list(),logTransform=FA
 		mlab=paste(orf,"doubling time:",dt,"h","inoculum:",inoc)
 		plot(x,y,xlab="Time (d)",ylab="log cell density (AU)",main=mlab)
 		abline(lA0,r,col="red")
-		plot(xnew,fitted(linfit),type="l",col="red",xlab="Time (d)",ylab="log cell density (AU)",main=mlab)
-		points(xnew,ynew)
-		plot(linfit,ask=asking)
+		if(length(xnew)>1){
+			plot(xnew,fitted(linfit),type="l",col="red",xlab="Time (d)",ylab="log cell density (AU)",main=mlab)
+			points(xnew,ynew)
+			plot(linfit,ask=asking)
+		}
 	}
 	return(pars)
 }
