55965f3024e12f5c9a2b844c0b1e2cdd6bb19f28
jheydari <jheydari@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-25 08:02:25 +0000

    jh
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qfa@179 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/qfaBayes/R/zzz.r
+++ pkg/qfaBayes/R/zzz.r
@@ -1,21 +1,4 @@
-.First.lib <- function(lib, pkg)
-{
-library.dynam("qfaBayes", pkg, lib)
-require(MASS)
-}
-
-.Last.lib <- function(lib, pkg)
-{
-library.dynam.unload("qfaBayes", pkg, lib)
-pos <- match("package:MASS", search())
-if(! is.na(pos)){
-detach(pos = pos)
-}
-}
-
-
 .onLoad <- function(lib, pkg){
 library.dynam("qfaBayes", pkg, lib)
-require(MASS)
      packageStartupMessage( "Bayesian QFA" )
 }
