ac74f84207797a6fa72e45fcde0f5fc4a883ecf7
jheydari (Mr ) <j.j.heydari@ncl.ac.uk>
2012-11-07 15:29:02 +0000

    jh
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qfa@240 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/qfaBayes/R/zzz.r
+++ pkg/qfaBayes/R/zzz.r
@@ -1,6 +1,9 @@
-.onLoad <- function(lib, pkg){
+".onLoad" <- function(lib, pkg)
+{
 library.dynam("qfaBayes", pkg, lib)
      packageStartupMessage( "
 Bayesian QFA 
 (independent C code avalible at https://github.com/jhncl/Model-in-C/tree/master/CCode_FINAL)" )
 }
+
+
