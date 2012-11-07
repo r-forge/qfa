900329c82e2497efd6b09a39fa03a238bd6a67bb
jheydari (Mr ) <j.j.heydari@ncl.ac.uk>
2012-11-07 11:21:56 +0000

    jh
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qfa@232 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/qfaBayes/src/headers.h
+++ pkg/qfaBayes/src/headers.h
@@ -1,14 +1,8 @@
 #include <stdio.h>
 #include <math.h>
-#include <gsl/gsl_randist.h>
 #include <stdlib.h>
-#include <gsl/gsl_rng.h>
-#include <gsl/gsl_math.h> 
-#include <gsl/gsl_sf_log.h>      
 #include <unistd.h>
 #include <time.h>
-#include <gsl/gsl_sf_gamma.h>
-#include <gsl/gsl_cdf.h>
 #include <string.h>
 #include <R.h>
 #include <Rmath.h>
--- pkg/qfaBayes/src/headers_JHM.h
+++ pkg/qfaBayes/src/headers_JHM.h
@@ -1,13 +1,8 @@
 #include <stdio.h>
 #include <math.h>
-#include <gsl/gsl_randist.h>
-#include <stdlib.h>
-#include <gsl/gsl_rng.h>
-#include <gsl/gsl_math.h> 
-#include <gsl/gsl_sf_log.h>      
+#include <stdlib.h>    
 #include <unistd.h>
 #include <time.h>
-#include <gsl/gsl_cdf.h>
 #include <R.h>
 #include <Rmath.h>
 
