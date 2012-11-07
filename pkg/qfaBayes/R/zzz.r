c297db15afec68767c5b33657853c9f07db4cc35
jheydari <jheydari@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-07 11:18:38 +0000

    jh
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qfa@231 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/qfaBayes/R/zzz.r
+++ pkg/qfaBayes/R/zzz.r
@@ -1,4 +1,6 @@
 .onLoad <- function(lib, pkg){
 library.dynam("qfaBayes", pkg, lib)
-     packageStartupMessage( "Bayesian QFA" )
+     packageStartupMessage( "
+Bayesian QFA 
+(independent C code avalible at https://github.com/jhncl/Model-in-C/tree/master/CCode_FINAL)" )
 }
