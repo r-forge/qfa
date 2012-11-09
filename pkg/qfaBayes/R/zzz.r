1f3b8d5d0d7ce6e653781bb79c516aef57709a22
jheydari (Mr ) <j.j.heydari@ncl.ac.uk>
2012-11-09 14:13:07 +0000

    jh
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qfa@268 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/qfaBayes/R/zzz.r
+++ pkg/qfaBayes/R/zzz.r
@@ -1,7 +1,7 @@
 ".onLoad" <- function(lib, pkg)
 {
   library.dynam(pkg, pkg, lib)
-     packageStartupMessage("\nBayesian QFA" 
+     packageStartupMessage("\nBayesian QFA", 
 "\nStart with demos SHM_C, IHM_C and JHM_C and then edit these scripts for results with suitable sample size, thinning and burn in.",
 "\nC code outside R avalible at https://github.com/jhncl/Model-in-C/tree/master/CCode_FINAL\n")
 }
