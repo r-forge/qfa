28e794bb0eab05a2ef3112e1d610770272044069
jheydari <jheydari@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-19 12:53:56 +0000

    jh
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qfa@294 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/qfaBayes/demo/SHM_simple_C.r
@@ -0,0 +1,9 @@
+data("URA3_Raw_trim")
+data("priors_SHM")
+PRIORS=as.double((priors_SHM)[[1]])[1:18]
+qfa.variables(a)
+
+Screen<-unique(a$Screen.Name)
+SHM<-SHM_postpro(a=a,Treat=27,Screen=Screen,MPlate=15)
+SHM_output<-SHM_main(burn=1,iters=1,thin=1,CAPL=4294,SHM$QFA.I,SHM$QFA.y,SHM$QFA.x,SHM$QFA.NoORF,SHM$QFA.NoTIME,PRIORS)
+
