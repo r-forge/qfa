31809cd3f5e3cd665128ec8ff5aa7137913fb086
jheydari (Mr ) <j.j.heydari@ncl.ac.uk>
2012-11-19 15:19:41 +0000

    jh
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qfa@301 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/qfaBayes/demo/SHM_simple_C.r
+++ pkg/qfaBayes/demo/SHM_simple_C.r
@@ -1,10 +1,12 @@
 data("URA3_Raw_extratrim_15")
 a<-a_15
+
 data("priors_SHM")
 PRIORS=as.double((priors_SHM)[[1]])[1:18]
 qfa.variables(a)
 
 Screen<-unique(a$Screen.Name)
+
 SHM<-SHM_postpro(a=a,Treat=27,Screen=Screen,MPlate=15)
 SHM_output<-SHM_main(burn=1,iters=1,thin=1,CAPL=50,QFA.I=SHM$QFA.I,QFA.y=SHM$QFA.y,QFA.x=SHM$QFA.x,QFA.NoORF=SHM$QFA.NoORF,QFA.NoTIME=SHM$QFA.NoTIME,PRIORS=PRIORS)
 
