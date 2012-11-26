88e7e64a758604be49375623169fb4c7297a1a22
jheydari <jheydari@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-26 10:57:00 +0000

    jh
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qfa@320 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/qfaBayes/demo/JHM_simple_C.r
+++ pkg/qfaBayes/demo/JHM_simple_C.r
@@ -20,7 +20,7 @@ MPlate_b<-as.character(unique(b$MasterPlate.Number))
 JHM<-JHM_postpro(a,TreatA=TreatA,Screen_a=Screen_a,MPlate_a,b,TreatB=TreatB,Screen_b,MPlate_b)
 JHM_output<-JHM_main(burn=1,iters=1,thin=1,QFA.IA=JHM$QFA.IA,QFA.yA=JHM$QFA.yA,QFA.xA=JHM$QFA.xA,QFA.NoORFA=JHM$QFA.NoORFA,QFA.NoTIMEA=JHM$QFA.NoTIMEA,QFA.IB=JHM$QFA.IB,QFA.yB=JHM$QFA.yB,QFA.xB=JHM$QFA.xB,QFA.NoORFB=JHM$QFA.NoORFB,QFA.NoTIMEB=JHM$QFA.NoTIMEB,PRIORS)
 
-ask_plot()
+ask_plot_simple()
 filename="DEMO"
 pdf(paste("JHM_plot_",filename,".pdf",sep=""),useDingbats=F)
 plot_JHM_simple(JHM_output,JHM)
