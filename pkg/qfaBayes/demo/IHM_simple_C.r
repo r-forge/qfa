b7a8941b558d4a692f01769bd159ad788c0c6546
jheydari <jheydari@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-04-18 19:43:45 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qfa@507 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/qfaBayes/demo/IHM_simple_C.r
+++ pkg/qfaBayes/demo/IHM_simple_C.r
@@ -16,13 +16,13 @@ Screen_a<-unique(a$Screen.Name)
 SHM_a<-SHM_postpro(a=a,Treat=27,Screen=Screen_a,MPlate=15)
 SHM_output_a<-SHM_main(burn=1,iters=1,thin=1,CAPL=50,QFA.I=SHM_a$QFA.I,
   QFA.y=SHM_a$QFA.y,QFA.x=SHM_a$QFA.x,QFA.NoORF=SHM_a$QFA.NoORF,
-  QFA.NoTIME=SHM_a$QFA.NoTIME,PRIORS=PRIORS,TUNING)
+  QFA.NoTIME=SHM_a$QFA.NoTIME,PRIORS=PRIORS,TUNING=TUNING)
 
 Screen_b<-unique(b$Screen.Name)
 SHM_b<-SHM_postpro(a=b,Treat=27,Screen=Screen_b,MPlate=15)
 SHM_output_b<-SHM_main(burn=1,iters=1,thin=1,CAPL=50,QFA.I=SHM_b$QFA.I,
   QFA.y=SHM_b$QFA.y,QFA.x=SHM_b$QFA.x,QFA.NoORF=SHM_b$QFA.NoORF,
-  QFA.NoTIME=SHM_b$QFA.NoTIME,PRIORS=PRIORS,TUNING)
+  QFA.NoTIME=SHM_b$QFA.NoTIME,PRIORS=PRIORS,TUNING=TUNING)
 
 data("priors_IHM")
 PRIORS_IHM=as.double((priors_IHM)[[1]])
@@ -34,7 +34,8 @@ SHM_b$QFA.yB=colMeans(SHM_output_b)
 
 IHM_output=IHM_main(burn=1,iters=1,thin=1,QFA.IA=SHM_a$QFA.I,
   QFA.yA=SHM_a$QFA.yA,QFA.NoORFA=SHM_a$QFA.NoORF,QFA.IB=SHM_b$QFA.I,
-  QFA.yB=SHM_b$QFA.yB,QFA.NoORFB=SHM_b$QFA.NoORF,PRIORS_IHM,TUNING_IHM)
+  QFA.yB=SHM_b$QFA.yB,QFA.NoORFB=SHM_b$QFA.NoORF,PRIORS=PRIORS_IHM,
+  TUNING=TUNING_IHM)
 
 ask_plot_simple()
 filename="DEMO"
