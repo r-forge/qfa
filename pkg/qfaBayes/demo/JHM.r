abeafd88743ae17ecb76f33dec7eae35754eadf7
jheydari (Mr ) <j.j.heydari@ncl.ac.uk>
2014-04-28 09:46:48 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qfa@514 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/qfaBayes/demo/JHM.r
+++ pkg/qfaBayes/demo/JHM.r
@@ -2,33 +2,37 @@ data("URA3_Raw_extratrim_15")#Control a
 a<-a_15
 data("CDC13-1_Raw_extratrim_15")#Query b 
 b<-b_15
+
 qfa.variables(a)
 qfa.variables(b)
 
 data("priors_JHM")
-PRIORS=as.double((priors_JHM)[[1]])
+PRIORS=priors_JHM[[1]]
 data("tuning_JHM")
-TUNING=as.double((tuning_JHM))[1:5]
+TUNING=tuning_JHM[[1]]
 
-TreatA=27
-Screen_a<-as.character(unique(a$Screen.Name))
-MPlate_a<-as.character(unique(a$MasterPlate.Number))
+Treat_a=27
+Screen_a<-unique(a$Screen.Name)
+MPlate_a<-15
 
-TreatB=27
-Screen_b<-as.character(unique(b$Screen.Name))
-MPlate_b<-as.character(unique(b$MasterPlate.Number))
+Treat_b=27
+Screen_b<-unique(b$Screen.Name)
+MPlate_b<-15
 
-JHM<-JHM_postpro(a,TreatA=TreatA,Screen_a=Screen_a,MPlate_a,b,TreatB=TreatB,
+burn<-1
+iters<-1
+thin<-1
+
+JHM<-JHM_postpro(a,TreatA=Treat_a,Screen_a=Screen_a,MPlate_a,b,TreatB=Treat_b,
   Screen_b,MPlate_b)
-JHM_output<-JHM_main(burn=1,iters=1,thin=1,QFA.IA=JHM$QFA.IA,QFA.yA=JHM$QFA.yA,
-  QFA.xA=JHM$QFA.xA,QFA.NoORFA=JHM$QFA.NoORFA,QFA.NoTIMEA=JHM$QFA.NoTIMEA,
-  QFA.IB=JHM$QFA.IB,QFA.yB=JHM$QFA.yB,QFA.xB=JHM$QFA.xB,
-  QFA.NoORFB=JHM$QFA.NoORFB,QFA.NoTIMEB=JHM$QFA.NoTIMEB,
+  
+JHM_output<-JHM_main(burn=burn,iters=iters,thin=thin,QFA.IA=JHM$QFA.IA,
+  QFA.yA=JHM$QFA.yA,QFA.xA=JHM$QFA.xA,QFA.NoORFA=JHM$QFA.NoORFA,
+  QFA.NoTIMEA=JHM$QFA.NoTIMEA,QFA.IB=JHM$QFA.IB,QFA.yB=JHM$QFA.yB,
+  QFA.xB=JHM$QFA.xB,QFA.NoORFB=JHM$QFA.NoORFB,QFA.NoTIMEB=JHM$QFA.NoTIMEB,
   PRIORS=PRIORS,TUNING=TUNING)
 
 ask_plot_simple()
-filename="DEMO"
-#pdf(paste("JHM_plot_",filename,".pdf",sep=""),useDingbats=F)
 plot_JHM_simple(JHM_output,JHM)
-#dev.off()
+
 
--- pkg/qfaBayes/demo/SHM.r
+++ pkg/qfaBayes/demo/SHM.r
@@ -1,26 +1,27 @@
 data("URA3_Raw_extratrim_15")
 a<-a_15
 
-data("priors_SHM")
-PRIORS=as.double((priors_SHM)[[1]])
-data("tuning_SHM")
-TUNING=as.double((tuning_SHM))[1:4]
 qfa.variables(a)
 
-Treat=27
-Screen<-unique(a$Screen.Name)
-MPlate=15
+data("priors_SHM")
+PRIORS<-priors_SHM[[1]]
+data("tuning_SHM")
+TUNING<-tuning_SHM[[1]]
 
-burn=1
-iters=1
-thin=1
+Treat<-27
+Screen<-unique(a$Screen.Name)
+MPlate<-15
+
+burn<-1
+iters<-1
+thin<-1
 
 SHM<-SHM_postpro(a=a,Treat=Treat,Screen=Screen,MPlate=MPlate)
+
 SHM_output<-SHM_main(burn=burn,iters=iters,thin=thin,QFA.I=SHM$QFA.I,
   QFA.y=SHM$QFA.y,QFA.x=SHM$QFA.x,QFA.NoORF=SHM$QFA.NoORF,
   QFA.NoTIME=SHM$QFA.NoTIME,PRIORS=PRIORS,TUNING=TUNING)
 
 ask_plot_simple()
-
 plot_SHM_simple(SHM_output,SHM)
 
