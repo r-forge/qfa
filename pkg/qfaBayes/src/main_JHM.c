c055eed5e486d6167f55eaf0e0b5d8d31fdc7ecf
jheydari <jheydari@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-07 11:10:04 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qfa@523 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/qfaBayes/src/main_JHM.c
+++ pkg/qfaBayes/src/main_JHM.c
@@ -6,7 +6,7 @@
 int main_JHM(int *arga,int *argb,int *argc,double *OUT, char **HEADER,
   int *QFAIA,double *QFADyA,double *QFADxA,int *QFADNoORFA,
   int *QFANoTIMEA,int *QFAIB,double *QFADyB,double *QFADxB,
-  int *QFADNoORFB,int *QFANoTIMEB,double *PRIORS,double *TUNING)
+  int *QFADNoORFB,int *QFANoTIMEB,double *PRIORS,double *TUNING)/*ADD ADAPTIVE 1,0*/
 {
   GetRNGstate();
   
@@ -14,20 +14,25 @@ int main_JHM(int *arga,int *argb,int *argc,double *OUT, char **HEADER,
   struct_para_JHM *para= malloc(sizeof(struct_para_JHM));
   struct_priors_JHM *priors= malloc(sizeof(struct_priors_JHM));
   struct_MH_JHM *MH = malloc(sizeof(struct_MH_JHM));
+  struct_tuning_JHM *tuning = malloc(sizeof(struct_tuning_JHM));
+  struct_adaptive_JHM *adaptive = malloc(sizeof(struct_tuning_JHM));
 
-  int burn,iters,thin;
-
+  int burn,iters,thin,adaptive_phase;
+/*ADD ADAPTIVE 1,0*/
   burn=*arga;   /*Burn in*/
   iters=*argb;    /*iterations*/
-  thin=*argc;        /*thining*/
+  thin=*argc;        /*thinning*/
+  adaptive_phase=100;
 
   inzstruct_data_JHM(data,QFAIA,QFADyA,QFADxA,QFADNoORFA,QFANoTIMEA,QFAIB,QFADyB,QFADxB,QFADNoORFB,QFANoTIMEB);
   inzstruct_MH_JHM(MH,TUNING);
+  inzstruct_tuning_JHM(tuning,TUNING);
+  inzstruct_adaptive_JHM(adaptive);
   inzstruct_priors_JHM(priors,PRIORS);
   inzstruct_para_JHM(para,data,priors);
 
-  gibbsandMHloop_JHM(burn,1,data,para,priors,MH,0,OUT,HEADER);
-  gibbsandMHloop_JHM(iters,thin,data,para,priors,MH,1,OUT,HEADER);
+  gibbsandMHloop_JHM(burn,1,data,para,priors,MH,tuning,adaptive,0,adaptive_phase,OUT,HEADER);/*ADD ADAPTIVE 1,0 tuning adaptive -MH*/
+  gibbsandMHloop_JHM(iters,thin,data,para,priors,MH,tuning,adaptive,1,0,OUT,HEADER);/*ADD ADAPTIVE 1,0 tuning adaptive -MH*/
 
   PutRNGstate();
   
