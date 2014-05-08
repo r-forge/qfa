6da2b8175fed88d4d7190e9c5dc783cfde32eb84
jheydari <jheydari@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-08 09:14:21 +0000

    correction
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qfa@538 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/qfaBayes/src/main_SHM.c
+++ pkg/qfaBayes/src/main_SHM.c
@@ -22,13 +22,13 @@ int main_SHM(int *arga,int *argb,int *argc,int *argd,int *arge,double *OUT,
   thin=*argc;         /*thinning*/
   adaptive_phase=*argd; /*adaptive phase length within burn in*/
   adaptive_period=*arge; /*period to calculate adaptive statistic over*/
-  
+
   inzstruct_data(data,QFAI,QFADy,QFADx,QFADNoORF,QFADNoTIME);
-  inzstruct_tuning_JHM(tuning,TUNING);
-  inzstruct_adaptive_JHM(adaptive);
+  inzstruct_tuning(tuning,TUNING);
+  inzstruct_adaptive(adaptive);
   inzstruct_priors(priors,PRIORS);
   inzstruct_para(para,data,priors);
-  
+
   gibbsandMHloop(burn,1,data,para,priors,tuning,adaptive,0,adaptive_phase,adaptive_period,OUT,HEADER);
   gibbsandMHloop(iters,thin,data,para,priors,tuning,adaptive,1,0,0,OUT,HEADER);
   
